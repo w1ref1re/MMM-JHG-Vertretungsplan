9aeed5dd1642ed3386c861f0b6ceafbc2154cfad
Fabi <fabian.hof25@gmail.com>
2019-12-29 20:54:17 +0100

    1.9-added-stylesheet

--- MMM-JHG-Vertretungsplan.js
+++ MMM-JHG-Vertretungsplan.js
@@ -75,7 +75,7 @@ Module.register("MMM-JHG-Vertretungsplan", {
         table.className = "table";
 
         var row = document.createElement("tr");
-        row.className = "small light bright headline";
+        row.className = "small thin bright headline";
 
         for (var headline in data[0]) {
             var th = document.createElement("th");
@@ -95,9 +95,9 @@ Module.register("MMM-JHG-Vertretungsplan", {
             var row = document.createElement("tr");
 
             if (i % 2 == 0) {
-                row.className = "small regular normal row";
+                row.className = "small thin normal row";
             } else {
-                row.className = "small regular bright row";
+                row.className = "small thin bright row";
             }
             
             for (var item in data[vertretung]) {
