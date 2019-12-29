e78ee11c0034387aee7fb825ae374e765c92ec58
Fabi <fabian.hof25@gmail.com>
2019-12-29 14:13:50 +0100

    1.6

--- node_helper.js
+++ node_helper.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const NodeHelper = require("node_helper");
-const spawn = require("child_process").spawn;
+//const spawn = require("child_process").spawn;
 
 module.exports = NodeHelper.create({
     start: function() {
@@ -11,12 +11,33 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
         switch(notification) {
             case "GET_VERTRETUNGEN":
+
+
+                const child_process = require('child_process');
+
+                const child = child_process.spawn("python3", ["fetch_jhg.py", "h", "4", "f"]);
+                
+                child.stdout.on('data', (data) => {
+                    console.log(`stdout: ${data}`);
+                    this.sendSocketNotification("GET_VERTRETUNGEN", data);
+                });
+                
+                child.stderr.on('data', (data) => {
+                    console.error(`stderr: ${data}`);
+                    this.sendSocketNotification("GET_VERTRETUNGEN", data);
+                });
+                
+                child.on('close', (code) => {
+                    console.log(`child process exited with code ${code}`);
+                });
+                
+                
                 
                 //var p = spawn("python3", [payload.script_path, payload.classes, payload.base_url, payload.home_url]);
                 
                 //var p = spawn("python3", ["fetch_jhg.py", "d", "f", "5"]);
 
-                var p = spawn("ls");
+               /* var p = spawn("ls");
 
                 p.stdout.on("data", (data) => {
                     this.sendSocketNotification("GET_VERTRETUNGEN", data);
@@ -24,7 +45,7 @@ module.exports = NodeHelper.create({
 
                 p.stderr.on("data", (data) => {
                     this.sendSocketNotification("GET_VERTRETUNGEN", data);
-                });
+                });*/
 
                 break;
         }
